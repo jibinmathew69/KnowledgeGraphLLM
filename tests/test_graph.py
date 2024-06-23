263fb1a03fbb3095a25032ee7460454f6b1009d8
jibinmathew69 <jibinmathew.69@gmail.com>
2024-06-24 02:10:32 +0530

    Create graph_document fixture

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -3,6 +3,9 @@ import os
 from langchain_community.graphs.graph_document import Relationship
 from langchain_community.graphs.graph_document import Node
 from langchain_anthropic import ChatAnthropic
+from langchain_community.graphs import Neo4jGraph
+from app.graph import create_graph
+from app.text_parser import chunk_pdf
 
 
 SAMPLE_PDF_PATH = os.path.abspath(
@@ -18,14 +21,12 @@ def sample_pdf_file():
     return SAMPLE_PDF_PATH
 
 
-def test_graph_document():
-    from app.graph import create_graph
-    from app.text_parser import chunk_pdf
-
+@pytest.fixture(scope="module")
+def graph_document():
     text_chunk = chunk_pdf(SAMPLE_PDF_PATH)
     text_chunk = text_chunk[:2]
 
-    graph_document = create_graph(
+    return create_graph(
         text_chunk,
         llm = ChatAnthropic(
             model="claude-3-5-sonnet-20240620",
@@ -35,8 +36,10 @@ def test_graph_document():
         )
     )
 
+
+def test_graph_document(graph_document):    
     assert isinstance(graph_document, list), "Result should be a list"    
-        
+
     assert all(
         isinstance(nodes, Node) for nodes in graph_document[0].nodes
     ), "All graph_document.nodes items should be Node objects"
@@ -44,4 +47,5 @@ def test_graph_document():
     assert all(
         isinstance(relationships, Relationship) for relationships in graph_document[0].relationships
     ), "All graph_document.relationships items should be Relationship objects"
-    
+
+
