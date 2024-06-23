dca1fc07c52170403d7d031631c7b68836e929bd
jibinmathew69 <jibinmathew.69@gmail.com>
2024-06-24 00:38:20 +0530

    Fix sample path

--- tests/test_text_parser.py
+++ tests/test_text_parser.py
@@ -3,11 +3,16 @@ import os
 from app.text_parser import chunk_pdf
 from langchain_core.documents.base import Document
 
-SAMPLE_PDF_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'samples', 'mango.pdf'))
+SAMPLE_PDF_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "sample", "Mango.pdf")
+)
+
 
 @pytest.fixture(scope="module")
 def sample_pdf_file():
-    assert os.path.exists(SAMPLE_PDF_PATH), f"Sample PDF file not found at {SAMPLE_PDF_PATH}"
+    assert os.path.exists(
+        SAMPLE_PDF_PATH
+    ), f"Sample PDF file not found at {SAMPLE_PDF_PATH}"
     return SAMPLE_PDF_PATH
 
 
@@ -16,7 +21,9 @@ def test_parse_pdf():
 
     assert isinstance(text_chunks, list), "Result should be a list"
 
-    assert all(isinstance(doc, Document) for doc in text_chunks), "All items should be Document objects"
+    assert all(
+        isinstance(doc, Document) for doc in text_chunks
+    ), "All items should be Document objects"
 
     assert len(text_chunks) == 7, f"Expected 7 documents, but got {len(text_chunks)}"
 
