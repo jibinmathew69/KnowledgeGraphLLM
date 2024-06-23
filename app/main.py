e6fd93bd9b9a5853737af4359bba094363363b3c
jibinmathew69 <jibinmathew.69@gmail.com>
2024-06-23 23:59:05 +0530

    Load env

--- app/main.py
+++ app/main.py
@@ -1,4 +1,20 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+required_env_vars = [
+    "NEO4J_URI",
+    "NEO4J_USERNAME",
+    "NEO4J_PASSWORD",
+    "OPENAI_API_KEY",
+    "ANTHROPIC_API_KEY"
+]
+
+for var in required_env_vars:
+    if not os.getenv(var):
+        print(f"Environment variable {var} is not set.")
+        exit(1)
 
 app = FastAPI()
 
