7c4db20e1dd2d7a7750c13b993d900ff508f7644
jibinmathew69 <jibinmathew.69@gmail.com>
2024-06-24 16:03:12 +0530

    Fix create embedding

--- app/embedding/graph_embedding.py
+++ app/embedding/graph_embedding.py
@@ -1,5 +1,4 @@
 from langchain_community.vectorstores import Neo4jVector
-from langchain_openai import OpenAIEmbeddings
 from langchain_core.documents.base import Document
 from langchain_core.embeddings import Embeddings
 from langchain.chains import RetrievalQA
