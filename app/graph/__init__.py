c7502ca8a3dd49434793d54d7d2b6e5eec81abaa
jibinmathew69 <jibinmathew.69@gmail.com>
2024-06-24 16:12:16 +0530

    Add graph qa chain

--- app/graph/__init__.py
+++ app/graph/__init__.py
@@ -1,3 +1,5 @@
 from .create_graph import create_graph
 from .write_graph import write_graph
 from .disambiguate import disambiguate
+from .qa import get_qa_chain as graph_qa_chain
+from .qa import get_answer as graph_answer
